26a531d813e4a3b3f2d798a75872ce1ae6a8b37b
blac16313-source <blac16313@gmail.com>
2026-09-15 11:33:15 +0530

    Update deploy.js

--- api/deploy.js
+++ api/deploy.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
       {
         access: "public",
         contentType: "text/html",
+        contentDisposition: "inline",
         addRandomSuffix: false
       }
     );
